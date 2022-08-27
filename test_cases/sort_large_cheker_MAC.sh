f2a9b7475c819146c44d229fa520c6d0cb100170
Ahmed Salem <ahsalem@lab1r6s8.42abudhabi.ae>
2022-08-27 13:21:35 +0400

    better tester

--- test_cases/sort_large_cheker_MAC.sh
+++ test_cases/sort_large_cheker_MAC.sh
@@ -4,13 +4,8 @@
 echo "please enter number of integers you want to generate"
 read n
 
-echo "#!/bin/bash" > generated_nums.sh
 make -C ../
-python3 generator.py $n >> generated_nums.sh
+echo "#!/bin/bash" > generated_nums.sh
+python3 checker_gen.py $n >> generated_nums.sh
 chmod +x generated_nums.sh
 ./generated_nums.sh
-echo "---------------------------------------------------"
-echo "Number of moves = $(./generated_nums.sh | wc -l) "
-
-echo -n "Checker result =        "
-./checker_generated_nums.sh
